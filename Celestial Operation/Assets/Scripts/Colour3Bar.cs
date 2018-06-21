ddee1138e6fffbe959edce81e1dac7a1563bdef6
Max Kemp <max.kemp@uoit.net>
2018-06-21 12:09:41 -0400

    Added a class to create display bars, with any number of colours. It will smootly transitions between all of the specified colours.

---
+++ Celestial Operation/Assets/Scripts/Colour3Bar.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Colour3Bar : MonoBehaviour
+{
+
+    [SerializeField]
+    private Color currentColour;
+    [SerializeField]
+    private Color fullColour;
+    [SerializeField]
+    private Color halfColour;
+    [SerializeField]
+    private Color emptyColour;
+
+    private Slider slider;
+    private Image fill;
+
+    private void Awake()
+    {
+        slider = GetComponent<Slider>();
+        fill = transform.Find("Fill Area").Find("Fill").GetComponent<Image>();
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+        currentColour = fullColour;
+        fill.color = currentColour;
+    }
+
+
+    public void UpdateColour()
+    {
+        // Change colour between full and half
+        if (slider.value > 50.0f)
+            currentColour = Color.Lerp(halfColour, fullColour, ((slider.value - 50.0f) / 50.0f));
+        // Change colour betweeen half and empty
+        else
+            currentColour = Color.Lerp(emptyColour, halfColour, (slider.value / 50.0f));
+
+        // Update the colour of the fill
+        fill.color = currentColour;
+
+    }
+
+}
--- Celestial Operation/Assets/Scripts/DisplayBar.cs
+++ Celestial Operation/Assets/Scripts/DisplayBar.cs
@@ -7,37 +7,76 @@ public class DisplayBar : MonoBehaviour
 {
     [SerializeField]
     private Color currentColour;
+
     [SerializeField]
-    private Color fullColour;
-    [SerializeField]
-    private Color halfColour;
-    [SerializeField]
-    private Color emptyColour;
+    private List<Color> colours;
 
     private Slider slider;
+    private Image fill;
+    [SerializeField]
+    private float maxValue;
+    private float valuePerColour;
+    private int numTransitions = 0;
 
 
     private void Awake()
     {
         slider = GetComponent<Slider>();
+        fill = transform.Find("Fill Area").Find("Fill").GetComponent<Image>();
     }
 
     // Use this for initialization
     void Start ()
     {
-        currentColour = fullColour;
-	}
-	
-	// Update is called once per frame
-	void Update ()
-    {
-		
+        // At least one colour in the list
+        if (colours.Count > 0)
+        {
+            // Set this bar's colour to the first colour in the list
+            currentColour = colours[0];
+            fill.color = currentColour;
+            //maxValue = slider.maxValue;
+
+            // At least two colours in the list
+            if (colours.Count > 1)
+            {
+                // Number of colour transitions is the total number of colours minus one
+                numTransitions = colours.Count - 1;
+                // Divide the max value of the slider amongst all the transitions
+                valuePerColour = maxValue / numTransitions;
+            }
+        }
 	}
 
-    void UpdateColour()
+    /// <summary>
+    /// Smoothly changes this bar's colour.
+    /// </summary>
+    public void UpdateColour()
     {
-        if (slider.value > 0.5f)
-            currentColour = Color.Lerp(fullColour, halfColour, ((slider.value - 0.5f) / 0.5f));
+        /* Find the correct two colours to lerp between, based on the current value of the slider and
+         * the number of colour transitions
+         */
+        for (int i = numTransitions; i > 0; i--)
+        {
+            // Value used to determine which two colours we are inbetween
+            float colourValue = valuePerColour * (i - 1);
+
+            // Inbetween last and second last colour
+            if (colourValue <= 0)
+            {
+                currentColour = Color.Lerp(colours[i - 1], colours[i], (slider.value / valuePerColour));
+                break;
+            }
+            // Inbetween two colours, non of which are the last colour
+            else if (slider.value > colourValue)
+            {
+                currentColour = Color.Lerp(colours[i - 1], colours[i], ((slider.value - colourValue) / valuePerColour));
+                break;
+            }
+        }
+
+        // Update the colour of the fill
+        fill.color = currentColour;
+
     }
 
 }
