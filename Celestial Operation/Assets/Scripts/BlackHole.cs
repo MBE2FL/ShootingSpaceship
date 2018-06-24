a64227b0fe7692482f0c625062b79053675e5a20
Max Kemp <maxkemp1111@hotmail.com>
2018-06-24 13:30:38 -0400

    Black hole now grows and shrinks.

--- Celestial Operation/Assets/Scripts/BlackHole.cs
+++ Celestial Operation/Assets/Scripts/BlackHole.cs
@@ -6,13 +6,15 @@ public class BlackHole : MonoBehaviour
 {
  
     private IEnumerator generateField;
-    [SerializeField]
-    private float fieldForce;
     private Rigidbody rb;
     [SerializeField]
     private float lifeTime;
     private float totalTime;
     private float elapsedTime;
+    private float halfTime;
+    [SerializeField]
+    private Vector3 maxScale;
+    private IEnumerator growShrink;
 
     private float radius;
     private float gravitationalConstant = 1.0f;
@@ -27,9 +29,13 @@ public class BlackHole : MonoBehaviour
     private void OnEnable()
     {
         totalTime = lifeTime + Time.time;
+        halfTime = (lifeTime * 0.5f) + Time.time;
 
         generateField = GenerateField();
-        StartCoroutine(generateField);
+        //StartCoroutine(generateField);
+
+        growShrink = GrowShrink();
+        StartCoroutine(growShrink);
     }
 
     // Use this for initialization
@@ -41,17 +47,18 @@ public class BlackHole : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
     {
-		
-	}
+        elapsedTime = Time.time;
+    }
 
     IEnumerator GenerateField()
     {
-        elapsedTime = Time.time;
-
+        // Generate gravitational field for the duration of this black holes life time
         while (elapsedTime <= totalTime)
         {
+            // Find all objects, with colliders attacted, in the vacinity of this black hole
             Collider[] colliders = Physics.OverlapSphere(transform.position, 10.0f);
 
+            // Pull in each collider
             foreach (Collider collider in colliders)
             {
                 if (collider.tag == "Enemy")
@@ -62,9 +69,31 @@ public class BlackHole : MonoBehaviour
                     Vector3 force = ((500.0f * 1.0f) / distance) * direction;
                     collider.GetComponent<Rigidbody>().AddForce(force);
 
-                    Debug.Log(force);
+                    //Debug.Log(force);
                 }
             }
+            yield return null;
+        }
+
+    }
+
+    IEnumerator GrowShrink()
+    {
+        Vector3 originalScale = transform.localScale;
+
+        // Grow until this black hole reaches half of it's life time
+        while (elapsedTime <= halfTime)
+        {
+            transform.localScale = Vector3.Lerp(originalScale, maxScale, (elapsedTime / halfTime));
+            Debug.Log(transform.localScale);
+            yield return null;
+        }
+
+        // Shrink once this black hole reaches half of it's life time
+        while (elapsedTime <= totalTime)
+        {
+            transform.localScale = Vector3.Lerp(maxScale, originalScale, (elapsedTime / totalTime));
+            Debug.Log(transform.localScale);
             yield return null;
         }
 
